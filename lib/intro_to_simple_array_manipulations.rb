9740ca08da3a4e5f44341ecd545343b0a88c8573
Kainan Doneson <kaindoneson@gmail.com>
2020-07-20 23:39:59 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(arr1, arr2)
+ return arr1.concat(arr2);
+end
+
+def using_insert(arr, el)
+  return arr.insert(4, el)
+end
+
+def using_uniq(array)
+  return array.uniq;
+end
+
+def using_flatten(arr)
+  return arr.flatten;
+end
+
+def using_delete(arr, el)
+  return arr.delete(el);
+end
+
+def using_delete_at(array, int)
+  return array.delete_at(int);
+end
+  
