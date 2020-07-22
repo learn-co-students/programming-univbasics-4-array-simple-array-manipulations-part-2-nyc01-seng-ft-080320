780b7fadfb40a7657caf4179277c2d4b87068e57
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-22 14:37:25 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_concat (array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert (array, index)
+  array.insert(4, index)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, item)
+  array.delete (item)
+end
+def using_delete_at (array, num)
+  array.delete_at(num)
+end
